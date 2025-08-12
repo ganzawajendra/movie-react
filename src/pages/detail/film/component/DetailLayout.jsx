0c409b9496a4add2d7e4ff5a1ea32dc6ab2b619f
ganzawajendra <ganzawjendra@gmail.com>
2025-08-12 11:53:18 +0700

    add: menambahkan content detail movie

--- src/pages/detail/film/component/DetailLayout.jsx
+++ src/pages/detail/film/component/DetailLayout.jsx
@@ -2,21 +2,21 @@ import React, { useState } from "react";
 import { Link } from "react-router";
 import OverviewDetails from "./ContentDetails/OverviewDetails";
 import TrailerDetails from "./ContentDetails/TrailerDetails";
+import CastDetails from "./ContentDetails/CastDetails";
+import MoreLikeThisDetails from "./ContentDetails/MoreLikeThisDetails";
 
 const DetailLayout = ({filmDetails}) => {
     const [content, setContent] = useState("overview");
-
-    const contentDetail = [
-        <OverviewDetails key="overview" filmDetails={filmDetails} />,
-        <TrailerDetails key="trailer" filmDetails={filmDetails} />
-    ]
-
     const renderContent = () => {
         switch (content) {
             case "overview":
                 return <OverviewDetails filmDetails={filmDetails} />;
             case "trailer":
                 return <TrailerDetails filmDetails={filmDetails} />;
+            case "cast":
+                return <CastDetails filmDetails={filmDetails} />;
+            case "moreLikeThis":
+                return <MoreLikeThisDetails filmDetails={filmDetails} />;
             default:
                 return null;
         }
@@ -31,10 +31,10 @@ const DetailLayout = ({filmDetails}) => {
         <button className={`cursor-pointer py-1 px-3 border-b-3 ${content === "trailer" ? "border-red-500" : "border-black"} w-full`} onClick={() => setContent("trailer")}>
             <h1 className="text-white font-montserrat">Trailers</h1>
         </button>
-        <button className="cursor-pointer py-1 px-3 border-b-3 w-full">
+        <button className={`cursor-pointer py-1 px-3 border-b-3 ${content === "moreLikeThis" ? "border-red-500" : "border-black"} w-full`} onClick={() => setContent("moreLikeThis")}>
             <h1 className="text-white font-montserrat">More Like This</h1>
         </button>
-        <button className="cursor-pointer py-1 px-3 border-b-3 w-full">
+        <button className={`cursor-pointer py-1 px-3 border-b-3 ${content === "cast" ? "border-red-500" : "border-black"} w-full`} onClick={() => setContent("cast")}>
             <h1 className="text-white font-montserrat">Details</h1>
         </button>
       </div>
