c63767992eae41f326719f857b2c52e5ee4ced04
ganzawajendra <ganzawjendra@gmail.com>
2025-08-18 00:33:06 +0700

    menambahkan list budget dan companies di overview details

--- src/pages/detail/film/component/ContentDetails/OverviewDetails.jsx
+++ src/pages/detail/film/component/ContentDetails/OverviewDetails.jsx
@@ -14,7 +14,14 @@ const OverviewDetails = ({ filmDetails, id }) => {
     getCreditsMovie(filmId)
       .then((data) => {
         setCast(data.cast);
-        setWriter(data.crew.filter((member) => member.job === "Writer" || member.job === "Story" || member.job === "Adaptation"));
+        setWriter(
+          data.crew.filter(
+            (member) =>
+              member.job === "Writer" ||
+              member.job === "Story" ||
+              member.job === "Adaptation"
+          )
+        );
         setIsLoading(false);
       })
       .catch((error) => {
@@ -38,25 +45,61 @@ const OverviewDetails = ({ filmDetails, id }) => {
   }
 
   return (
-    <div className="px-3">
-      <p className="text-white ">{filmDetails.overview}</p>
-      <div className="flex items-center gap-4 mt-10 justify-start">
+    <div className="px-3 flex flex-col gap-10">
+      {/* Overview */}
+      <p className="text-white font-lato">{filmDetails.overview}</p>
+
+      {/* Starring, Genres, dan Writers */}
+      <div className="flex items-center gap-4 justify-start">
         <div className="flex flex-col gap-3">
-          <p className="text-gray-500 font-montserrat-light text-sm">Starring</p>
-          <p className="text-gray-500 font-montserrat-light text-sm">Genres</p>
-          <p className="text-gray-500 font-montserrat-light text-sm">Writers</p>
+          <p className="text-gray-500 font-montserrat-light">
+            Starring
+          </p>
+          <p className="text-gray-500 font-montserrat-light">Genres</p>
+          <p className="text-gray-500 font-montserrat-light">Writers</p>
         </div>
         <div className="flex flex-col gap-3">
-          <span className="text-white font-montserrat-light text-sm line-clamp-1">
+          <span className="text-white font-lato line-clamp-1">
             {cast.map((member) => member.name).join(", ")}
           </span>
-          <span className="text-white font-montserrat-light text-sm">
+          <span className="text-white font-lato">
             {filmDetails.genres.map((genre) => genre.name).join(", ")}
           </span>
-          <span className="text-white font-montserrat-light text-sm">
-            {writer.map((member) => member.original_name).join(", ") ? writer.map((member) => member.original_name).join(", ") : "..."}
+          <span className="text-white font-lato">
+            {writer.map((member) => member.original_name).join(", ")
+              ? writer.map((member) => member.original_name).join(", ")
+              : "..."}
           </span>
         </div>
+      </div>
+
+      {/* Budget */}
+      <div className="flex flex-col gap-1">
+        <h1 className="text-white max-w-max px-2 font-lato border-b-3 border-red-500">
+          Budget
+        </h1>
+        <span className="text-white font-montserrat">
+          {filmDetails.budget
+            ? `${filmDetails.budget.toLocaleString("en-US", {
+                style: "currency",
+                currency: "USD",
+              })}`
+            : "Not Mentioned"}
+        </span>
+      </div>
+
+      {/* Companies */}
+      <div className="flex flex-col gap-1">
+        <h1 className="text-white max-w-max px-2 font-lato border-b-3 border-red-500">
+          Companies
+        </h1>
+        <span className="text-white font-montserrat">
+          {filmDetails.production_companies.length > 0
+            ? filmDetails.production_companies
+                .map((company) => company.name)
+                .join(", ")
+            : "Not Mentioned"}
+        </span>
       </div>
     </div>
   );
